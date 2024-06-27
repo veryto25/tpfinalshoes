c67cd6aa11f6b8c202407c46d00e35985676363a
veryto25 <vepersano@gmail.com>
2024-06-27 16:07:51 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const shoesRouter = require('../routes/shoes')
+const cors = require ('cors')
+
+app.use(cors()); 
 
 app.use(express.json())
 
