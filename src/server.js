14540d58c79c9f282620559d6bf1eb74d4188e24
veryto25 <vepersano@gmail.com>
2024-06-28 09:10:03 -0300

    indicar puerto 5500 en server, solucion fallida accesos a imagenes bloqueo corb

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@ const express = require('express')
 const app = express()
 const shoesRouter = require('../routes/shoes')
 const cors = require ('cors')
+const whiteList = ['http://127.0.0.1:5500']
+// Constante cors para abrir app desde otro puerto (5500)
 
-app.use(cors()); 
+app.use(cors({ origin: whiteList })); 
 
-app.use(express.json())
+// solucionar apertura de imagen en html ( no funicionó!!!!!)
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+   
+  });
 
-app.use('/shoes', shoesRouter)
+app.use(express.json());
+
+app.use('/shoes', shoesRouter);
+
 
 const PORT = 3000
 app.listen(PORT, () => {
