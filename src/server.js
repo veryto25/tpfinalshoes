d55f74b26a149231b5285f16f4afcf12f71acb42
veryto25 <vepersano@gmail.com>
2024-07-05 17:38:31 -0300

    adecuacion puerto env para deployar

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ const paisesRouter = require('../routes/paises')
 var bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
-const whiteList = ['http://127.0.0.1:5500']
+import 'dotenv/config';
+
+//const whiteList = ['http://127.0.0.1:5500'] esto lo uso para el local host
 // Constante cors para abrir app desde otro puerto (5500)
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(cors({ origin: whiteList }));
+app.use(cors());
+// app.use(cors({ origin: whiteList }));
 app.use(express.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -23,7 +25,7 @@ app.use('/shoes', shoesRouter);
 app.use('/creadores',creadoresRouter);
 app.use('/paises',paisesRouter);
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`)
 });
